b5ad8c3f436d9583b05553d0fc98ebd031e07bd5
DannyThomx <dannithomx@gmail.com>
2024-05-16 00:42:09 +0100

    clean up Utilities.sol

--- packages/hardhat/contracts/libraries/Utilities.sol
+++ packages/hardhat/contracts/libraries/Utilities.sol
@@ -7,13 +7,29 @@ import "./storage/LibRefereeStorage.sol";
 import "./storage/LibAppStorage.sol";
 import "@unlock-protocol/contracts/dist/PublicLock/IPublicLockV12.sol";
 
+/**
+ * @title Utilities Library
+ * @dev Provides utility functions used across multiple contracts
+ */
 library Utilities {
 
-  	function _getMembershipLock() internal view returns (address) {
+    /**
+     * @notice Returns Unadus the Membership contract address
+     * @dev Fetches the address of the Membership Lock from AppStorage
+     * @return Address of the Membership Lock
+     */
+	function _getMembershipLock() internal view returns (address) {
 		AppStorage storage s = LibAppStorage.diamondStorage();
 		return s.membershipLock;
 	}
 
+    /**
+     * @notice Returns the referrers of a specific campaign
+     * @dev Fetches the address of Level 2 and Level 3 referrers for a specific campaign
+     * @param _levelOneReferrerAddress The address of the first level referrer
+     * @param _campaignId The ID of the campaign
+     * @return The addresses of the second and third level referrers
+     */
     function _getMultiLevelReferrers(address _levelOneReferrerAddress, address _campaignId) view internal returns(address, address) {
 		RefereeStorage storage refereeStorage = LibRefereeStorage.diamondStorage();
 		address levelTwoReferrer = refereeStorage.referrerOf[_levelOneReferrerAddress][_campaignId];
@@ -21,12 +37,22 @@ library Utilities {
 		return (levelTwoReferrer, levelThreeReferrer);
 	}
 
-	function _isLockManager(
-		address _lockAddress
-	) internal view returns (bool isManager) {
+    /**
+     * @notice Checks if the sender is a lock manager of a particular lock
+     * @dev Uses the isLockManager method from IPublicLockV12 to verify if the sender is a manager of the lock
+     * @param _lockAddress the address of the lock
+     * @return isManager value indicating whether the sender is a lock manager or not
+     */
+	function _isLockManager(address _lockAddress) internal view returns (bool isManager) {
 		isManager = IPublicLockV12(_lockAddress).isLockManager(msg.sender);
 	}
-
+  
+    /**
+     * @notice Checks if a user has valid memberbership NFT for Unadus membership contract
+     * @dev Uses the getHasValidKey method from IPublicLockV12 to verify if the user has a valid key 
+     * @param _user Address of the user to be checked
+     * @return _hasValidMembership value indicating whether the user is a member or not
+     */
     function _isMember(address _user) 
 		internal 
 		view returns (bool _hasValidMembership) 
@@ -36,6 +62,12 @@ library Utilities {
 		_hasValidMembership = IPublicLockV12(membershipLock).getHasValidKey(_user);
 	}
 
+    /**
+     * @notice Updates referee storage
+     * @dev Updates the refereeData and referrerOf mapping and allReferees list in the storage
+     * @param _referee Data related to the referee
+     * @param _referrer The address of the referrer
+     */
     function _updateRefereeStorage(
 		RefereeInfo memory _referee,
 		address _referrer
@@ -46,7 +78,7 @@ library Utilities {
 		_refereeStorage.refereeData[_referee.id][
 			_referee.campaignId
 		] = _referee;
-		// if referrer not zero address add as the referrer
+		// if referrer is not zero address, add as the referrer
 		if (_referrer != address(0))
 			_refereeStorage.referrerOf[_referee.id][
 				_referee.campaignId
@@ -58,5 +90,4 @@ library Utilities {
 			_appStorage.isReferee[_referee.id] = true;
 		}
 	}
-	
 }
